a0803bd4b2328fe0c9e15d7e50e562ad7db62b3f
NoelArizonPanjaitan <noelari2004@gmail.com>
2023-09-22 09:47:13 +0700

    kuis_1

---
+++ 3.pas
@@ -0,0 +1,18 @@
+program biodata_pribadi;
+uses crt;
+var
+    nama,alamat,kom:string;
+    nim:longint;
+begin
+clrscr;
+    write('Masukkan Nama Anda: ');
+    readln(nama);
+    write('Masukkan NIM Anda: ');
+    readln(nim);
+    write('Masukkan KOM Anda: ');
+    readln(kom);
+    write('Masukkan Alamat Anda: ');
+    readln(alamat);
+writeln('Hallo, nama saya ', nama, ' NIM saya: ',nim, ' saya di KOM ',kom , ' Alamat saya di ',alamat, '.Salam kenal semua!');
+readln;
+end.
