be32d767eaa95a1099acfa105be8a5059538274e
NoelArizonPanjaitan <145732065+NoelArizonPanjaitan@users.noreply.github.com>
2023-09-28 18:58:38 +0700

    Add files via upload

---
+++ kuis_1_A_Noel_057.pas
@@ -0,0 +1,19 @@
+program biodata_pribadi;
+uses crt;
+var
+    nama,alamat:string;
+    kom:char;
+    nim:longint;
+ begin
+    clrscr;
+    write('Masukkan Nama Anda: ');
+    readln(nama);
+    write('Masukkan NIM Anda: ');
+    readln(nim);
+    write('Masukkan KOM Anda: ');
+    readln(kom);
+    write('Masukkan Alamat Anda: ');
+    readln(alamat);
+    writeln('Hallo, nama saya ', nama, ' NIM saya:',nim, ' saya di KOM ',kom , ' Alamat saya di ',alamat, '.Salam kenal semua!');
+    readln;
+end.
