123e87c5bf0937c085a08fae62df873ec8c09ae7
NoelArizonPanjaitan <145732065+NoelArizonPanjaitan@users.noreply.github.com>
2023-09-28 19:02:42 +0700

    Add files via upload

---
+++ kuis_1_B_Noel_057.pas
@@ -0,0 +1,18 @@
+program alamatLengkap;
+uses crt;
+var
+    a,c:string;
+    b,d:integer;
+ begin 
+    clrscr;
+    write('Jalan: ');
+    readln(a);
+    write('NO.: ');
+    readln(b);
+    write('Kota: ');
+    readln(c);
+    write('Kode POS: ');
+    readln(d);
+    write('Jalan ', a, ' No.', b ,' ', c ,' ', d);
+    readln;
+end.
